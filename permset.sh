e44cd6d0a3f4ea12539eb9351e195b1a9b59b159
Tats Shibata <ts@rewse.jp>
2014-04-09 23:49:54 +0900

    Add permset.sh
    permset.sh sets permissions, owners, groups and contexts all at once.

---
+++ permset.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR=/srv/www/wpdev
+
+chown -R apache:tats $DIR
+
+chmod 02775 $DIR
+find $DIR -type d -print0 | xargs -0 chmod 02775
+find $DIR -type f -print0 | xargs -0 chmod 664
+find $DIR -name \*.sh -print0 | xargs -0 chmod 775
+
+restorecon -R $DIR
