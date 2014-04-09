23987b27f782f1b8672473746c601fa45a0f8c04
Tats Shibata <ts@rewse.jp>
2014-04-10 00:58:02 +0900

    Change the file owners from apache to tats
    Because apache should not have write permission.

--- permset.sh
+++ permset.sh
@@ -2,12 +2,18 @@
 
 DIR=/srv/www/wpdev
 
-chown -R apache:tats $DIR
+chown -R tats:apache $DIR
 
-chmod 02775 $DIR
-find $DIR -type d -print0 | xargs -0 chmod 02775
-find $DIR -type f -print0 | xargs -0 chmod 664
-find $DIR -name \*.sh -print0 | xargs -0 chmod 775
-chmod 660 $DIR/wp-db-config.php
+chmod 02755 $DIR
+find $DIR -type d -print0 | xargs -0 chmod 02755
+find $DIR -type f -print0 | xargs -0 chmod 644
+find $DIR -name \*.sh -print0 | xargs -0 chmod 755
+
+chown root:root $DIR/permset.sh
+
+chmod 640 $DIR/wp-db-config.php
+chmod 02775 $DIR/wp-content/plugins
+chmod 02775 $DIR/wp-content/themes
+chmod 02775 $DIR/wp-content/upgrade
 
 restorecon -R $DIR
