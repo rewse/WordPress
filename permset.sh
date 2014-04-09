e86de8e20ac95b04d458c6f8fb1137426cbe7b1a
Tats Shibata <ts@rewse.jp>
2014-04-10 00:21:33 +0900

    Create wp-config.php via web
    wp-config.php should not be tracked because it holds a database
    connection password. But the other configs should be tracked.
    
    The database connection configs is moved to untracked wp-db-config.php.
    wp-db-config.php is ignored (untracked) and other users can not write.

--- permset.sh
+++ permset.sh
@@ -8,5 +8,6 @@ chmod 02775 $DIR
 find $DIR -type d -print0 | xargs -0 chmod 02775
 find $DIR -type f -print0 | xargs -0 chmod 664
 find $DIR -name \*.sh -print0 | xargs -0 chmod 775
+chmod 660 $DIR/wp-db-config.php
 
 restorecon -R $DIR
