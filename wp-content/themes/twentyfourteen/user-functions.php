e2212909a5024ffb2a84452fb0021d2915469934
Tats Shibata <ts@rewse.jp>
2014-04-13 02:21:27 +0900

    Support TypeSquare
    See also <https://typesquare.com/help/index/3>.

---
+++ wp-content/themes/twentyfourteen/user-functions.php
@@ -0,0 +1,6 @@
+<?php
+function rewse_typesquare_head() {
+  echo '<script type="text/javascript" src="//typesquare.com/accessor/script/typesquare.js?T-oMA8MY0BQ%3D" charset="utf-8"></script>';
+}
+add_action('wp_head', 'rewse_typesquare_head')
+?>
