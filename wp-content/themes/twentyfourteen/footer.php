2ae83db1b6524858374a589570ec74ee7c093ad1
Tats Shibata <ts@rewse.jp>
2014-04-16 00:43:38 +0900

    Add a message about the customized source coude

--- wp-content/themes/twentyfourteen/footer.php
+++ wp-content/themes/twentyfourteen/footer.php
@@ -18,7 +18,8 @@
 
 			<div class="site-info">
 				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>, <a href="http://httpd.apache.org/">The Apache HTTP Server</a>, <a href="http://www.mysql.com/">MySQL</a>, <a href="http://www.oracle.com/jp/technologies/linux/overview/">Oracle Linux</a> and <a href="http://www.supermicro.com/products/motherboard/xeon/qm77/x9spv-f-3217ue.cfm">Supermicro X9SPV-F-3217UE</a>
+        <p><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>, <a href="http://httpd.apache.org/">The Apache HTTP Server</a>, <a href="http://www.mysql.com/">MySQL</a>, <a href="http://www.oracle.com/jp/technologies/linux/overview/">Oracle Linux</a> and <a href="http://www.supermicro.com/products/motherboard/xeon/qm77/x9spv-f-3217ue.cfm">Supermicro X9SPV-F-3217UE</a><br />
+        The customized WordPress source code is published at <a href="https://github.com/rewse/WordPress">GitHub</a></a>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
