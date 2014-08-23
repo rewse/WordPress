e8497b2f44fab5f95ddf1e634de651176f1dcb92
Tats Shibata <ts@rewse.jp>
2014-08-24 02:03:03 +0900

    Remove Jetpack's Open Graph meta tags
    Because they are coflicted with All In One SEO Pack.

--- wp-content/themes/twentyfourteen/user-functions.php
+++ wp-content/themes/twentyfourteen/user-functions.php
@@ -3,4 +3,8 @@ function rewse_typesquare_head() {
   echo '<script type="text/javascript" src="//typesquare.com/accessor/script/typesquare.js?T-oMA8MY0BQ%3D" charset="utf-8"></script>';
 }
 add_action('wp_head', 'rewse_typesquare_head');
+
+# Remove Jetpack's Open Graph meta tags
+# See also <http://jetpack.me/2013/05/03/remove-open-graph-meta-tags/>
+add_filter( 'jetpack_enable_open_graph', '__return_false' );
 ?>
