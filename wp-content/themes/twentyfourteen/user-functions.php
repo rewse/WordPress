32fcb9a89295de98e9e8350e459ab4624d969cb9
Tats Shibata <ts@rewse.jp>
2014-09-24 00:07:35 +0900

    Use a subset of SourceHanSansJP for titles instead of TypeSquare
    Because the PV reaches the limit of TypeSquare free plan constantly.

--- wp-content/themes/twentyfourteen/user-functions.php
+++ wp-content/themes/twentyfourteen/user-functions.php
@@ -2,7 +2,7 @@
 function rewse_typesquare_head() {
   echo '<script type="text/javascript" src="//typesquare.com/accessor/script/typesquare.js?T-oMA8MY0BQ%3D" charset="utf-8"></script>';
 }
-add_action('wp_head', 'rewse_typesquare_head');
+#add_action('wp_head', 'rewse_typesquare_head');
 
 # Remove Jetpack's Open Graph meta tags
 # See also <http://jetpack.me/2013/05/03/remove-open-graph-meta-tags/>
